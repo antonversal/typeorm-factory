55940bc5e0509b8c2bb83ee82c39968f7e225991
Anton Versal <ant.ver@gmail.com>
2017-11-27 20:46:02 +0100

    public method documentation

--- src/Factory.ts
+++ src/Factory.ts
@@ -4,10 +4,16 @@ import { AssocOneAttribute } from "./AssocOneAttribute";
 import { FactoryAttribute } from "./FactoryAttribute";
 import { Sequence } from "./Sequence";
 
+/**
+ * Interface for a class
+ */
 export interface IConstructable<T> {
   new(): T;
 }
 
+/**
+ * Attribute type
+ */
 export type Attr<U> = [keyof U,
   Sequence<U[keyof U]> |
   AssocManyAttribute<any> |
@@ -17,13 +23,23 @@ export type Attr<U> = [keyof U,
 export type Attrs<U> = Array<Attr<U>>;
 
 /**
- * Factory defines attributes, sequences and associations
+ * Factory defines attributes, sequences and associations for an Entity
  */
 export class Factory<T> {
+
+  /**
+   * typeorm Entity class
+   */
   public Entity: IConstructable<T>;
+
+  /**
+   * array of attributes generators
+   */
   public attrs: Attrs<T>;
+
   private privateRepository: Repository<T>;
 
+  /** constructor */
   constructor(Entity: IConstructable<T>, attrs?: Attrs<T> ) {
     this.Entity = Entity;
     this.attrs = attrs || [];
@@ -34,45 +50,69 @@ export class Factory<T> {
     return this.privateRepository;
   }
 
+  /**
+   * Clones current factory definition
+   */
   public clone(): Factory<T> {
     const clonedAttrs = this.attrs.map<Attr<T>>(([name, attr]) => [name, attr.clone()]);
     return new Factory<T>(this.Entity, clonedAttrs);
   }
 
+  /**
+   * sequence generator
+   */
   public sequence<K extends keyof T>(name: K, sequenceFunction: (i: number) => T[K]): Factory<T> {
     this.attrs.push([name, new Sequence<T[keyof T]>(sequenceFunction)]);
     return this;
   }
 
+  /**
+   * attribute generator
+   */
   public attr<K extends keyof T>(name: K, value: T[K]): Factory<T> {
     const clonedFactory = this.clone();
     clonedFactory.attrs.push([name, new FactoryAttribute<T[keyof T]>(value)]);
     return clonedFactory;
   }
 
-  // here is `any` for factory attribute. Typescript does not understand T[K][0]
+  // here is `any` for factory attribute definition. Typescript does not understand T[K][0]
+  /**
+   * association generator for one hasMany
+   */
   public assocMany<K extends keyof T>(name: K, factory: Factory<any>, size: number = 1): Factory<T> {
     const clonedFactory = this.clone();
     clonedFactory.attrs.push([name, new AssocManyAttribute(factory, size)]);
     return clonedFactory;
   }
 
+  /**
+   * association generator for one hasMany
+   */
   public assocOne<K extends keyof T>(name: K, factory: Factory<T[K]>): Factory<T> {
     const clonedFactory = this.clone();
     clonedFactory.attrs.push([name, new AssocOneAttribute(factory)]);
     return clonedFactory;
   }
 
+  /**
+   * builds an instance of Entity
+   */
   public build(attributes: Partial<T> = {}): T {
     const ignoreKeys = Object.keys(attributes);
     const obj = this.assignAttrs(new this.Entity(), ignoreKeys);
     return this.repository.merge(obj, attributes as any);
   }
 
+  /**
+   * builds a list instances of Entity
+   */
   public buildList(size: number): T[] {
     return Array.from({ length: size }, () => this.build());
   }
 
+  /**
+   * creates an Entity
+   */
   public async create(attributes: Partial<T> = {}): Promise<T> {
     const ignoreKeys = Object.keys(attributes);
     const obj = await this.assignAsyncAttrs(new this.Entity(), ignoreKeys);
@@ -80,6 +120,9 @@ export class Factory<T> {
     return this.repository.save(objWithAttrs as any);
   }
 
+  /**
+   * creates a list of Entities
+   */
   public async createList(size: number): Promise<T[]> {
     const objects = Array.from({ length: size }, () => this.create());
     const list = await Promise.all(objects);
